254c53aa908c2575fb73f0ab398ccca2ff858ec4
Steve Wight <stevendouglaswight@gmail.com>
2016-11-24 18:36:14 -0800

    Initial Commit

---
+++ DuoKit/DuoKit.h
@@ -0,0 +1,19 @@
+//
+//  DuoKit.h
+//  DuoKit
+//
+//  Created by Steve on 11/24/16.
+//  Copyright © 2016 Steve Wight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DuoKit.
+FOUNDATION_EXPORT double DuoKitVersionNumber;
+
+//! Project version string for DuoKit.
+FOUNDATION_EXPORT const unsigned char DuoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DuoKit/PublicHeader.h>
+
+
